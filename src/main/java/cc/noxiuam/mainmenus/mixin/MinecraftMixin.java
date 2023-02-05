d0b1271e2824aaa77e4d37adf86d9a360d5f32b3
Nox <xxevilmarcelxx@gmail.com>
2023-02-05 10:48:37 -0500

    Fix: Crash when the mod is disabled

--- src/main/java/cc/noxiuam/mainmenus/mixin/MinecraftMixin.java
+++ src/main/java/cc/noxiuam/mainmenus/mixin/MinecraftMixin.java
@@ -14,10 +14,12 @@ public abstract class MinecraftMixin {
 
     @Inject(method = "displayGuiScreen", at = @At(value = "HEAD"), cancellable = true)
     private void onGuiDisplay(GuiScreen guiScreenIn, CallbackInfo ci) {
-        if (guiScreenIn instanceof GuiMainMenu
-                || (guiScreenIn == null && Minecraft.getMinecraft().theWorld == null)) {
-            ci.cancel();
-            Minecraft.getMinecraft().displayGuiScreen(MainMenus.config.getMainMenu());
+        if (MainMenus.config.enabled) {
+            if (guiScreenIn instanceof GuiMainMenu
+                    || (guiScreenIn == null && Minecraft.getMinecraft().theWorld == null)) {
+                ci.cancel();
+                Minecraft.getMinecraft().displayGuiScreen(MainMenus.config.getMainMenu());
+            }
         }
     }
 
